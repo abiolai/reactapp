58d3cdda510754f7356a3814921dab5baebb852c
abiola <abiola@moatcohorts.com.ng>
2025-10-26 17:49:49 +0100

    updated vite config

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/abiolai/", // add this line to include your repo name, the two slashes important
+  base: "/reactapp/", // add this line to include your repo name, the two slashes important
 })
